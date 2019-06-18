7e56d2ae04d26dfed6ab00cac6ce0fde2301ce2b
bretonio <gringolalia@gmail.com>
2019-06-18 11:07:27 -0300

    W3CSS from Scratch a WIP

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+#app
+	.w3-bar.w3-blue-grey.w3-large.w3-padding-24
+		a.w3-bar-item.w3-button.w3-hover-black(style="font-size: 21px;")
+			i.fas.fa-chess.fa-2x.w3-margin-right
+			| Engenho Novo Versão Xadrez
+		a.w3-bar-item.w3-button.w3-hover-black.w3-right.w3-margin-right San Pavlov
+		a.w3-bar-item.w3-button.w3-hover-black.w3-right.w3-margin-right Ubatuba
+		a.w3-bar-item.w3-button.w3-hover-black.w3-dropdown-hover.w3-right.w3-margin-right
+			| Sambodia &nbsp;
+			i.material-icons expand_more
+			.w3-dropdown-content.w3-bar-block.w3-card-4
+				a.w3-bar-item.w3-button(href='#')
+					i.fas.fa-sun.fa-lg.w3-margin-right
+					| Samba
+				a.w3-bar-item.w3-button(href='#')
+					i.fas.fa-star-and-crescent.fa-lg.w3-margin-right
+					| Choro
+				a.w3-bar-item.w3-button(href='#')
+					i.fas.fa-mercury.fa-lg.w3-margin-right
+					| Marchinhas
+				a.w3-bar-item.w3-button(href='#')
+					i.fas.fa-fighter-jet.fa-lg.w3-margin-right
+					| Pagode
+				a.w3-bar-item.w3-button(href='#')
+					i.fas.fa-space-shuttle.fa-lg.w3-margin-right
+					| Baião
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-moon.fa-lg.w3-margin-right
+					| Frevo
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-mercury.fa-lg.w3-margin-right
+					| Forró
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-mosque.fa-lg.w3-margin-right
+					| Xote
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-broadcast-tower.fa-lg.w3-margin-right
+					| Maracatú
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-chess-knight.fa-lg.w3-margin-right
+					|Coco de Repente
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-tv.fa-lg.w3-margin-right
+					| Hip-Hop
+				a.w3-bar-item.w3-button(href='#')
+					i.fa.fa-users.fa-lg.w3-margin-right
+					| Bossa Nova
+	router-view
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style>
+#app {
+	font-family: 'News Cycle', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+</style>
---
+++ src/components/velha-guarda.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+.hello
+	img(src='../assets/hammer-sickle.svg' width="175px", style="margin-top: 10vh")
+	h1 Adeus, Mundo Imundo
+	h2 Wisdom of Worldly Wobblies
+	h3 Cloned in Sambodia 2018
+
+	button.w3-button.w3-red.w3-large.w3-margin-top.w3-hover-blue-grey.w3-ripple
+		i.material-icons all_inclusive
+		| &nbsp; Impress Me
+
+	button.w3-button.w3-blue-grey.w3-large.w3-margin-top.w3-hover-black.w3-margin-left(onclick="document.getElementById('id01').style.display='block'")
+		i.material-icons beach_access
+		| &nbsp; Life, A Beach
+
+	#id01.w3-modal
+		.w3-modal-content.w3-card-1.w3-left-align(style="max-width: 500px")
+			header.w3-container.w3-blue-grey
+				span.w3-button.w3-display-topright.w3-hover-grey.w3-xlarge(onclick="document.getElementById('id01').style.display='none'") &times;
+
+				h2 Mensagem Que Aparece Oba
+
+			.w3-container
+				p Λορεμ ιπσθμ δολορ σιτ αμετ, περ ερθδιτι σιγνιφερθμqθε ει, εθμ σολθτα ιντερεσσετ αccομμοδαρε cθ. Qθι σονετ επιcθρει ιδ. Εθ μει σαλε qθοδσι. Qθισ βρθτε μελιθσ προ?
+
+			footer.w3-container.w3-blue-grey
+				p Fonte: Falha de S. Paulo
+
+
+</template>
+
+
+<script>
+	var modal = document.getElementById('id01');
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+	font-weight: normal;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
